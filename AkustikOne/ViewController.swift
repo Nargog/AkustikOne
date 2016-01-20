10c026f83b07590db1027056b85a408c87fc7640
Mats Hammarqvist <mats.hammarqvist@comhem.se>
2016-01-20 21:52:01 +0100

    Initial Commit

---
+++ AkustikOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AkustikOne
+//
+//  Created by Mats Hammarqvist on 2016-01-20.
+//  Copyright © 2016 Mats Hammarqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
